e3cdb5c5cb12c4f69f75e6ba73ee3666f49eafb4
Justin Leitgeb <justin@stackbuilders.com>
2015-06-04 10:22:46 -0500

    Be more clear about parse checks for syntax versus semantics.

--- lib/simple_templates/parser.rb
+++ lib/simple_templates/parser.rb
@@ -1,58 +1,91 @@
 module SimpleTemplates
+
+  # Parsing the SimpleTemplate means verifying the *syntax* and *semantics* of
+  # the template. That is, it shouldn't have malformed tags, and all tags should
+  # be in the tag whitelist.
   class Parser
 
     # After parsing, we get a data structure containing `Placeholder`s
     # and `String`s or an `Array` containing a single `Error`.
-    Placeholder = Struct.new(:name)
-
     UNESCAPES = {
       lt:  '<',
       gt:  '>',
       esc: '\\'
     }.freeze
 
-    Error = Struct.new(:message)
+    PH_TAGS = [:ph_start, :ph_end]
 
-    attr_reader :placeholder_names
+    FRIENDLY_TAG_NAMES = {
+      ph_start: 'placeholder start',
+      ph_end:   'placeholder end',
+      lt:       'less than',
+      gt:       'greater than',
+      esc:      'escape',
+      text:     'text'
+    }.freeze
 
-    def initialize(raw_template, placeholder_names)
-      @tokens            = Lexer.new(raw_template).tokenize
-      @placeholder_names = placeholder_names
+    Placeholder = Struct.new(:name, :pos)
+    Error       = Struct.new(:message)
+
+    attr_reader :whitelisted_placeholders, :tokens
+
+    def initialize(raw_template, whitelisted_placeholders)
+      @tokens                   = Lexer.new(raw_template).tokenize
+      @whitelisted_placeholders = whitelisted_placeholders
     end
 
+    # Returns either a stream of valid tokens (Placeholders and Strings),
+    # or an Array containing one or more Errors.
     def parse
       toks = tokens.clone
 
       template_nodes = []
 
+      # This section strictly analyzes the *syntax* of the tokens.
       while toks.any?
-        case res = placeholder(toks)
-        when Placeholder  # We got a valid placeholder.
-          toks = toks[3..-1] # pop off the tokens we just used.
-          template_nodes << res
+        ps = PlaceholderSyntax.new(toks)
 
-        # We found an invalid placeholder, so just return this error.
-        when Error then return [ res ]
-
-        when NilClass # This token is *not* a placeholder.
-
-          # Return an error if we find a closing bracket outside of a placeholder.
-          if toks[0].type == :ph_end
-            return [ Error.new("Unexpected closing bracket found at position #{toks[0].pos}.") ]
+        if ps.applicable?
+          case res = ps.placeholder
+          when Placeholder
+            toks = toks[3..-1] # pop off the tokens we just used.
+            template_nodes << res
+          else
+            # In this case there is a syntactical error, so we don't proceed to
+            # do validation of placeholder names since we can't tell what they
+            # are with invalid tag syntax! Just return the first syntax error.
+            return [res]
           end
-
-          # No other tokens are invalid if we're not in a placeholder, so push
-          # this one on after casting as a basic text node.
+        else
           template_nodes << unescape(toks.shift)
-
-        else raise "Probable bug in placeholder detection, please report."
         end
       end
 
-      compress_adjacent_text_nodes(template_nodes)
+      # At this point we can do minor cleanup of our structure.
+      template_nodes = compress_adjacent_text_nodes(template_nodes)
+
+      # This section verifies the *semantics* of the token stream. In this case,
+      # all we care about is that the tokens are in the whitelist.
+      invalids = invalid_placeholders(template_nodes)
+      return invalid_placeholder_errors(invalids) unless invalids.empty?
+
+      template_nodes
     end
 
     private
+
+    def invalid_placeholder_errors(invalid_pholders)
+      invalid_pholders.map do |p|
+        Error.new("Invalid placeholder with name, '#{p.name}' found starting at position #{p.pos}.")
+      end
+    end
+
+    # Invalid placeholders are ones that are not explicitly whitelisted.
+    def invalid_placeholders(template_nodes)
+      template_nodes.select do |tn|
+        tn.is_a?(Placeholder) && !whitelisted_placeholders.include?(tn.name)
+      end
+    end
 
     def unescape(token)
       UNESCAPES[token.type] || token.content
@@ -60,7 +93,9 @@ module SimpleTemplates
 
     def compress_adjacent_text_nodes(template_nodes)
       template_nodes.reduce([]) do |compressed, node|
-        if !compressed.empty? && compressed.last.is_a?(String) && node.is_a?(String)
+        if !compressed.empty? &&
+          compressed.last.is_a?(String) && node.is_a?(String)
+
           compressed[0..-2] << compressed[-1] + node
         else
           compressed << node
@@ -68,29 +103,54 @@ module SimpleTemplates
       end
     end
 
-    def placeholder(toks)
-      if toks[0].type == :ph_start
-        if toks[1] && toks[1].type == :text
-          if placeholder_names.include?(toks[1].content)
-            if toks[2] && toks[2].type == :ph_end
-              Placeholder.new(toks[1].content)
-            else
-              err = toks[2] ? "Expected closing tag for placeholder which started at position #{toks[0].pos}, but found a #{toks[2].type} instead." :
-                      "No closing tag found for placeholder which started at position #{toks[0].pos}."
+    class PlaceholderSyntax
+      attr_reader :tag_tokens
 
-              Error.new(err)
-            end
-          else
-            Error.new("Invalid placeholder name '#{toks[1].content}' found at position #{toks[1].pos}.")
-          end
-        else
-          Error.new("Expected to find a valid placeholder name at #{toks[1].pos}, but found a #{toks[1].type} instead.")
-        end
-      else
-        nil # the toks at the head of this list do not represent a placeholder sequence.
+      # We take any sequence of three tokens to see if it's a valid Placeholder.
+      # NB: this excludes placeholder names containing escaped sequences. If
+      # that becomes necessary later, we'll have to change this approach.
+      def initialize(toks)
+        @tag_tokens = toks[0..2].compact
       end
-    end
 
-    attr_reader :tokens
+      EXPECTED_TAG_ORDER = [:ph_start, :text, :ph_end]
+
+      # If this token sequence starts with a placeholder tag, we can verify
+      # whether or not it's valid.
+      def applicable?
+        PH_TAGS.include?(tag_types.first)
+      end
+
+      # If this stream starts with a placeholder token, parse out the
+      # Placeholder, or return an Error indicating the syntax problem.
+      def placeholder
+        raise ArgumentError,
+          "Stream does not contain any placeholder tags!" unless applicable?
+
+        expected_order_with_found_tokens = EXPECTED_TAG_ORDER.zip(@tag_tokens)
+
+        expected_order_with_found_tokens.each do |expected_type, found_tag|
+          if found_tag.nil?
+            return Error.new("Expected #{FRIENDLY_TAG_NAMES.fetch(expected_type)} token, but reached end of input.")
+
+          elsif expected_type != found_tag.type
+            return Error.new("Expected #{FRIENDLY_TAG_NAMES.fetch(expected_type)} token at character position #{found_tag.pos}, but found a #{FRIENDLY_TAG_NAMES.fetch(found_tag.type)} token instead.")
+
+          else # This token was expected at this point in the placeholder sequence.
+          end
+        end
+
+        Placeholder.new(tag_name.content, tag_start.pos)
+      end
+
+      private
+      def tag_types
+        tag_tokens.map(&:type)
+      end
+
+      def tag_start ; @tag_tokens[0] ; end
+      def tag_name  ; @tag_tokens[1] ; end
+      def tag_end   ; @tag_tokens[2] ; end
+    end
   end
 end
--- test/simple_templates/parser_test.rb
+++ test/simple_templates/parser_test.rb
@@ -6,14 +6,14 @@ require 'ostruct'
 describe SimpleTemplates::Parser do
   describe "#parse" do
     it "parses a simple valid template" do
-      pholder = SimpleTemplates::Parser::Placeholder.new('bar')
+      pholder = SimpleTemplates::Parser::Placeholder.new('bar', 4)
 
       SimpleTemplates::Parser.new('foo <bar>', ['bar']).parse.
         must_equal ['foo ', pholder]
     end
 
     it "compresses adjacent text nodes after unescaping" do
-      pholder = SimpleTemplates::Parser::Placeholder.new('bar')
+      pholder = SimpleTemplates::Parser::Placeholder.new('bar', 7)
 
       SimpleTemplates::Parser.new('foo \< <bar>', ['bar']).parse.
         must_equal ['foo < ', pholder]
@@ -21,37 +21,44 @@ describe SimpleTemplates::Parser do
 
     it "returns an error when an opening bracket is found without a closing bracket" do
       SimpleTemplates::Parser.new('foo < <bar>', ['bar']).parse.must_equal [
-        SimpleTemplates::Parser::Error.new("Invalid placeholder name ' ' found at position 5.")
+        SimpleTemplates::Parser::Error.new("Expected placeholder end token at character position 6, but found a placeholder start token instead.")
       ]
     end
 
     it "returns an error when a closing bracket is found before an opening bracket" do
       SimpleTemplates::Parser.new('foo > <bar>', ['bar']).parse.must_equal [
-        SimpleTemplates::Parser::Error.new('Unexpected closing bracket found at position 4.')
+        SimpleTemplates::Parser::Error.new('Expected placeholder start token at character position 4, but found a placeholder end token instead.')
       ]
     end
 
     it "returns an error when an invalid placeholder name is found" do
       SimpleTemplates::Parser.new('foo <baz>', ['bar']).parse.must_equal [
-        SimpleTemplates::Parser::Error.new("Invalid placeholder name 'baz' found at position 5.")
+        SimpleTemplates::Parser::Error.new("Invalid placeholder with name, 'baz' found starting at position 4.")
+      ]
+    end
+
+    it "returns an multiple errors when there are multiple invalid placeholders" do
+      SimpleTemplates::Parser.new('foo <baz> <buz>', []).parse.must_equal [
+        SimpleTemplates::Parser::Error.new("Invalid placeholder with name, 'baz' found starting at position 4."),
+        SimpleTemplates::Parser::Error.new("Invalid placeholder with name, 'buz' found starting at position 10.")
       ]
     end
 
     it "returns an error when multiple opening brackets are found" do
       SimpleTemplates::Parser.new('foo <<baz>', ['bar']).parse.must_equal [
-        SimpleTemplates::Parser::Error.new("Expected to find a valid placeholder name at 5, but found a ph_start instead.")
+        SimpleTemplates::Parser::Error.new("Expected text token at character position 5, but found a placeholder start token instead.")
       ]
     end
 
     it "returns an error when a closing tag is expected, but an opening tag is found" do
       SimpleTemplates::Parser.new('foo <bar<>', ['bar']).parse.must_equal [
-        SimpleTemplates::Parser::Error.new("Expected closing tag for placeholder which started at position 4, but found a ph_start instead.")
+        SimpleTemplates::Parser::Error.new("Expected placeholder end token at character position 8, but found a placeholder start token instead.")
       ]
     end
 
     it "returns an error when a tag is not closed before the end of the input" do
       SimpleTemplates::Parser.new('foo <bar', ['bar']).parse.must_equal [
-        SimpleTemplates::Parser::Error.new("No closing tag found for placeholder which started at position 4.")
+        SimpleTemplates::Parser::Error.new("Expected placeholder end token, but reached end of input.")
       ]
     end
 
