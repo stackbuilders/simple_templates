c2b0f9996d489e00c0884349039d3063b6271639
Michal Papis <mpapis@gmail.com>
2015-06-16 22:02:40 +0200

    remove to early require from test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,6 @@
 require "coveralls"
 require "simplecov"
 
-require_relative "../lib/simple_templates"
-
 SimpleCov.start do
   formatter SimpleCov::Formatter::MultiFormatter[
     SimpleCov::Formatter::HTMLFormatter,
