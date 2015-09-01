051acb94c80846faa1e33b0f5ea76564293fb2bc
Justin Leitgeb <justin@stackbuilders.com>
2015-09-01 15:10:45 -0700

    Remove unnecessary dynamic programming
    Tests pass with less fancy code, so removing for now. We can re-add in
    a pull request that contains test and examples of how we want the code
    to be extended so that we can be sure the complexity is justified.

--- lib/simple_templates/AST/placeholder.rb
+++ lib/simple_templates/AST/placeholder.rb
@@ -3,12 +3,6 @@ require 'simple_templates/AST/node'
 module SimpleTemplates
   module AST
     class Placeholder < Node
-      module TemplateMethods
-        def placeholders
-          ast.select{ |node| SimpleTemplates::AST::Placeholder === node }.to_set
-        end
-      end
-
       def render(context)
         context.public_send(contents)
       end
--- lib/simple_templates/template.rb
+++ lib/simple_templates/template.rb
@@ -3,9 +3,6 @@ require 'set'
 module SimpleTemplates
 
   # A `Template` is a renderable collection of SimpleTemplates::AST nodes.
-  #
-  # AST classes are scanned for TemplateMethods module, if it's there, Template
-  # is extended with this module.
   class Template
 
     attr_reader :ast, :errors, :remaining_tokens
@@ -14,15 +11,6 @@ module SimpleTemplates
       @ast              = ast.clone.freeze if errors.empty?
       @errors           = errors.clone.freeze
       @remaining_tokens = remaining_tokens.clone.freeze
-
-      [
-       SimpleTemplates::AST::Placeholder,
-       SimpleTemplates::AST::Text
-      ].each do |ast_class|
-        if ast_class.const_defined?(:TemplateMethods)
-          extend(ast_class.const_get(:TemplateMethods))
-        end
-      end
     end
 
     # Returns all placeholder names used in the template.
@@ -36,6 +24,10 @@ module SimpleTemplates
     def render(context)
       raise errors unless errors.empty?
       ast.map { |node| node.render(context) }.join
+    end
+
+    def placeholders
+      ast.select{ |node| SimpleTemplates::AST::Placeholder === node }.to_set
     end
 
     def ==(other)
