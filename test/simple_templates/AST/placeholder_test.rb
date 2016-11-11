f48923f735783d0f901ebe9423e1f516e13c8951
Agustin Camino <agustin.camino@gmail.com>
2016-11-11 17:17:19 -0500

    Tidy up placeholder_test
    - Use `#` to make it explicit the test describes an instance method.
    - Put the example that corresponds to the happy path in the beginning.
    - Remove unnecessary parens from lambda expression. When there are no arguments
      it is safe to ommit the parens.

--- test/simple_templates/AST/placeholder_test.rb
+++ test/simple_templates/AST/placeholder_test.rb
@@ -1,28 +1,26 @@
-require_relative "../../test_helper"
+require_relative '../../test_helper'
 
 describe SimpleTemplates::AST::Placeholder do
   let(:target)  {SimpleTemplates::AST::Placeholder}
 
-  describe "render" do
-
+  describe '#render' do
     let(:substitutions) { { my_placeholder: 'result1' } }
 
-    it "fails when the placeholder name is not in the given Hash" do
-      ->(){
+    it 'succeeds' do
+      target.new('my_placeholder', 0, true).
+        render(substitutions).must_equal("result1")
+    end
+
+    it 'fails if the placeholder name is not in the given Hash' do
+      -> {
         target.new('not_in_substitutions', 0, true).render(substitutions)
       }.must_raise SimpleTemplates::AST::InterpolationError
     end
 
-    it "fails when the placeholder is marked as invalid" do
-      ->(){
+    it 'fails if the placeholder is marked as invalid' do
+      -> {
         target.new('my_placeholder', 0, false).render(substitutions)
       }.must_raise RuntimeError
     end
-
-    it "succeeds" do
-      target.new('my_placeholder', 0, true).
-        render(substitutions).must_equal("result1")
-    end
   end
-
 end
